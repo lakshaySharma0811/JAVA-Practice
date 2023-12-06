d72ef0a5acad711ccfb4ad0d35d1030a28cdad29
lakshaySharma0811 <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-06 23:43:44 +0530

    Create Variables .java

---
+++ Variables .java
@@ -0,0 +1,42 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+
+
+        // Primitive data types:
+        byte a = 12;
+        System.out.println("byte: " + a);
+
+        short b = -32768;
+        System.out.println("short: " + b);
+
+        int c = 785468875;
+        System.out.println("int: " + c);
+
+        long d = 854617878;
+        System.out.println("long: " + d);
+
+        // -----------------------------------------------------------
+
+        float e = 12.55555555555f; // Upto 5 digits
+        System.out.println("float: " + e);
+
+        double f = 12.3333333333333333333; // Upto 15 digits
+        System.out.println("double: " + f);
+
+        // -----------------------------------------------------------
+
+        boolean g = true; // True or False
+        System.out.println("boolean: " + g);
+
+        // -----------------------------------------------------------
+
+        char h = 'B';
+        System.out.println("char: " + h);
+
+        // -----------------------------------------------------------
+
+        // Non-Primitive data types: 
+        // classes and methods
+    }
+}
