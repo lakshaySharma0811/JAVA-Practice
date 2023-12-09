d3cc89f88bbe6bd0468a33aa1c207a6910ed5587
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-09 20:00:12 +0530

    Create Literals.java

---
+++ Literals.java
@@ -0,0 +1,26 @@
+public class Literals {
+    public static void main(String[] args) {
+        // Integer literals :-
+
+        // Binary (0, 1)
+        // int binaryNum = 0b100; // 0b represents binary
+        // System.out.println(binaryNum);
+        // (1 * 2^2) + (0 * 2^1) + (0 * 2^0) = 4
+
+        // Decimal (0-9)
+        // int deciNum = 120;
+        // System.out.println(deciNum);
+        // (1 * 10^2) + (2 * 10^1) + (0 * 10^0) = 120
+
+        // octal (0-7)
+        // int octalNum = 025;
+        // System.out.println(octalNum);
+        // (0 * 8^2) + (2 * 8^1) + (5 * 8^0) = 21
+
+        // hexa Decimal (A,B,C,D,E,F / 0-9)
+        // int hexaDeciNum = 0x2F; // 0x represents hexadecimal
+        // System.out.println(hexaDeciNum);
+        // (2 * 16^1) + (15 * 16^0) = 47
+
+    }
+}
