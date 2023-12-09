d31c359f429f0bfccac4fbe5393d4ce237ee0a51
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-09 11:37:10 +0530

    Update VariablesQues.java

--- VariablesQues.java
+++ VariablesQues.java
@@ -106,5 +106,19 @@ public class VariablesQues {
         // System.out.println("Square of the number is " + square + ", Cube of the number is " + cube
         //         + " and Fourth Power of the number is " + fourthPower);
         // sc.close();
+
+
+        // 10. Write a Java program to break an integer into a sequence of digits.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the 6-digit number: ");
+        // int num = sc.nextInt();
+        // int n1 = (num / 100000) % 10;
+        // int n2 = (num / 10000) % 10;
+        // int n3 = (num / 1000) % 10;
+        // int n4 = (num / 100) % 10;
+        // int n5 = (num / 10) % 10;
+        // int n6 = (num / 1) % 10;
+        // System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6);
+        // sc.close();
     }
 }
