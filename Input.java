f207714742aff11304b67e47d02bfebb7d75cde2
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-10 14:42:36 +0530

    Update Input.java

--- Input.java
+++ Input.java
@@ -4,9 +4,13 @@ public class Input {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int num = sc.nextInt();
-        System.out.println("The input number is: " + num);
-        sc.close();
+        // System.out.print("Enter a number: ");
+        // int num = sc.nextInt();
+        // System.out.println("The input number is: " + num);
+
+        // System.out.print("Enter the string: ");
+        // String str = sc.next();
+        // System.out.println("The input string is: " + str);
+        // sc.close();
     }
 }
