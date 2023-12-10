c507cebd355fc3810fa76febab473125a0c4fd37
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-10 20:09:35 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,29 @@
+
+public class Operators {
+    public static void main(String[] args) {
+        // Operators :-
+
+        // 1. Arithmetic operators
+
+        // Binary operators (for 2 operands)
+        // +, -, *, /, %
+        // int a = 10;
+        // int b = 5;
+        // System.out.println("Sum: " + (a + b));
+        // System.out.println("Difference: " + (a - b));
+        // System.out.println("Product: " + (a * b));
+        // System.out.println("Quotient: " + (a / b));
+        // System.out.println("Remainder: " + (a % b));
+
+        // Unary operators (for 1 operand)
+        // Pre increment (++a), Pre decrement (--a), Post increment(a++), Post
+        // decrement(a--)
+        // int a = 5;
+        // System.out.println(a++); // First assign the value then increase
+        // System.out.println(++a); // First increase then assign the value
+        // System.out.println(a--); // First assign the value then decrease
+        // System.out.println(--a); // First decrease then assign the value
+
+
+    }
+}
