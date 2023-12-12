9a03cae4bb8a24e1120b62e1214d61aeac956d0b
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-13 00:07:51 +0530

    Update IfElseQues.java

--- IfElseQues.java
+++ IfElseQues.java
@@ -5,15 +5,15 @@ public class IfElseQues {
 
         // 1. Write a Java program to get a number from the user and print whether it is
         // positive or negative.
-        Scanner sc = new Scanner(System.in);
-        double num = sc.nextInt();
-        if ((num) * (-1) == num) {
-            System.out.println("negative");
-        } else if ((num) * (-1) == -num) {
-            System.out.println("positive");
-        } else {
-            System.out.println("neither positive nor negative");
-        }
-        sc.close();
+        // Scanner sc = new Scanner(System.in);
+        // double num = sc.nextInt();
+        // if (num < 0) {
+        //     System.out.println("negative");
+        // } else if (num >= 0) {
+        //     System.out.println("positive");
+        // } else {
+        //     System.out.println("Error");
+        // }
+        // sc.close();
     }
 }
