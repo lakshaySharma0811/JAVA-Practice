7ec0611782d318e26f1e637b659640ff92cb2698
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-23 13:28:10 +0530

    Update Rectangle.java

--- ClassesAndObjects_2/Rectangle.java
+++ ClassesAndObjects_2/Rectangle.java
@@ -3,4 +3,9 @@ public class Rectangle extends Shape {
     public Rectangle(int length, int breadth) {
         super(length, breadth);
     }
+
+    void checkLength() {
+        System.out.println(this.length);
+        System.out.println(super.length);
+    }
 }
