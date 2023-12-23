b45d25b2a9253c1703857f54b8d66a09703a9bf9
Lakshay <151620943+lakshaySharma0811@users.noreply.github.com>
2023-12-23 13:29:09 +0530

    Update Square.java

--- ClassesAndObjects_2/Square.java
+++ ClassesAndObjects_2/Square.java
@@ -3,4 +3,12 @@ public class Square extends Shape {
     public Square(int side) {
         super(side, side);
     }
+
+    void checkLength() {
+        // this.length = 50;
+        super.length = 40;
+        System.out.println(this.length);
+        System.out.println(super.length);
+        return;
+    }
 }
